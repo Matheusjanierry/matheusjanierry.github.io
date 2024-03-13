ff9a6329db0c3952a44b2073ba0687eff65abc36
mathews <matheushoriaw@gmail.com>
2024-03-13 09:03:39 -0300

    só della

---
+++ core.js
@@ -0,0 +1,39 @@
+async function buscar(){
+    let procura = await fetch("lista.json")
+    let produtos = await procura.json()
+
+    let listaDiv = document.getElementById("lista-card")
+
+    for(let produto of produtos){
+        listaDiv.innerHTML += `
+            <div class="card" data-id="${produto.id}">
+                <div class="grupo-img">
+                    <img src="${produto.img[0]}"
+                    width="250" height="auto">
+                </div>
+                <div class= "textos">    
+                    <h3> ${produto.nome}</h3>
+                    <p> ${produto.descricao}</p>
+                    </div>
+                        <span class= "comDesconto"> R$ ${(produto.valorComDesconto).toFixed(2).replace(".", " ,")}</span>
+                        <span class= "semDesconto"> R$ ${(produto.valorSemDesconto).toFixed(2).replace(".", " ,")}</span>
+                    </div>
+                </div> 
+            </div>         
+        `
+    }
+    let elememtosCards = document.querySelectorAll(".card")
+    for(let card of elememtosCards){
+        card.addEventListener("click", cliquei)
+    }
+
+    
+}
+buscar()
+function cliquei(){
+    let elementoID = this.getAttribute("data-id")
+    window.location.href = "exe.html?mathewssouza=" + elementoID
+}
+
+cliquei()
+ 
---
+++ exe.js
@@ -0,0 +1,54 @@
+async function buscar(){
+    let buscar = await fetch("lista.json")
+    let produtos = await buscar.json()
+    
+    let parametrosURL = new URLSearchParams(window.location.search)
+    let idProduto = parametrosURL.get("mathewssouza")
+
+    let inProduto = null
+    for(let x in produtos){
+        if(produtos[x].id == idProduto ){
+            inProduto = x
+        }
+    }
+
+    document.title = produtos[inProduto].nome
+
+    document.getElementById("detalhes").innerHTML+=`
+        <h1>
+            ${produtos[inProduto].nome}    
+        </h1>
+        <img src="${produtos[inProduto].img[0]}" id="img-frame"  width="300" height="auto" style="border:1px solid #000;border-radius:10px;">
+        <div class= "mini-img" id="mini-img">
+                
+        </div>
+        <p>
+            ${produtos[inProduto].descricao}
+        </p>
+        <div class="grupoValores">
+                <span class="ComDesconto">
+                    R$${(produtos[inProduto].valorComDesconto).toFixed(2).replace(".",",")}
+                </span>
+                <span class="SemDesconto">
+                    R$${(produtos[inProduto].valorSemDesconto).toFixed(2).replace(".",",")}
+                </span>
+        </div>
+       
+    `
+    let divMiniaturas = document.getElementById("mini-img")
+    for(let x of produtos[inProduto].img){
+        divMiniaturas.innerHTML += `
+            <img src="${x}" class="miniatura" width="80" height="80" style="border:1px solid #000;border-radius: 5px;">
+        `
+    }
+
+    let listaMiniaturas = document.getElementsByClassName("miniatura")
+    for(let card of listaMiniaturas){
+        card.addEventListener("mouseover", alteraImg)
+    }
+    function alteraImg(){
+        document.getElementById("img-frame").src = this.getAttribute("src")
+    }
+   
+}
+buscar()
